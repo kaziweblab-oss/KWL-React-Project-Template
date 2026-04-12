e3767c9fd09604642f3189dd4619dd26dc4472e5
Kazi Tasin Hossen <kaziweblab@gmail.com>
2026-04-12 13:21:33 +0600

    Update Templeate and add React-Helmet

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet";
 import HeroSection from "../../components/Home/HeroSection";
 import style from "./HomePage.module.css";
 export default function HomePage() {
   return (
     <>
+      <Helmet>
+        <title>KWL React Project Template</title>
+      </Helmet>
       <section className={style.HeroSection}>
         <HeroSection />
       </section>
